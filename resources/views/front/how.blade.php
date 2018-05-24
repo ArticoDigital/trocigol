e2566cb6db4049739bb686def2a6325c5c369529
juan2ramos <juan2ramos@gmail.com>
2018-05-24 18:38:53 -0500

    coach

--- resources/views/front/how.blade.php
+++ resources/views/front/how.blade.php
@@ -1,11 +1,18 @@
 @extends('layouts.front')
 @section('content')
-    <main class="How row justify-center align-end" id="How">
-        <figure class="How-title">
-            <img src="{{asset('images/como jugar.png')}}" alt="">
-        </figure>
-        <figure id="Coach">
-            <img src="{{asset('images/profe.png')}}" alt="">
-        </figure>
-    </main>
+	<main class="How row justify-center align-end" id="How">
+		<figure class="back-coach">
+			<a href="{{url('jugar')}}">
+				<img src="{{asset('images/back.png')}}" alt="">
+			</a>
+		</figure>
+		<figure class="How-title">
+			<img src="{{asset('images/como-jugar-text.png')}}" alt="">
+		</figure>
+		<figure id="Coach">
+
+			<img src="{{asset('images/como-jugar.png')}}" alt="">
+
+		</figure>
+	</main>
 @endsection
