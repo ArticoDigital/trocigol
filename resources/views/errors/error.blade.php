c1405a89b7e246c713dda884bbdb172c58739403
juan <juan2ramos@gmail.com>
2018-06-14 21:06:36 -0500

    error

---
+++ resources/views/errors/error.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.front')
+@section('content')
+	<main class="How row" id="How">
+		<figure id="Coach">
+
+			<img src="{{asset('images/error.blade.php')}}" alt="">
+
+		</figure>
+	</main>
+@endsection
