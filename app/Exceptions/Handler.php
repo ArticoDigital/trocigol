560476986fab1044eb97d5849c3f4d38ff66dba0
juan <juan2ramos@gmail.com>
2018-06-15 08:39:40 -0500

    error

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -48,7 +48,7 @@ class Handler extends ExceptionHandler
     public function render($request, Exception $exception)
     {
 
-        return response()->redirectTo('/');
+        return response()->view('errors.error', [], 500);
 
         return parent::render($request, $exception);
     }
